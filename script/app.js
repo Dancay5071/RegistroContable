7961e5fe3151d062a8a4572513f56380d9f30c18
= <daniinad175.may30@gmail.com>
2024-09-03 09:57:43 -0300

    configuracion de resumen

--- script/app.js
+++ script/app.js
@@ -8,86 +8,103 @@ let totalAhorros = parseFloat(localStorage.getItem('totalAhorros')) || 0;
 let ahorrosPorMes = JSON.parse(localStorage.getItem('ahorrosPorMes')) || {};
 let yearSeleccionado = new Date().getFullYear();
 
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    const yearSelect = document.getElementById('inputGroupSelectYear');
-    const currentYear = new Date().getFullYear();
-    const startYear = 2024;
-    const endYear = currentYear + 2;
+    
+    const pageId = document.body.id;
 
-    for (let year = startYear; year <= endYear; year++) {
-        let option = document.createElement('option');
-        option.value = year;
-        option.textContent = year;
-        if (year === currentYear) {
-            option.selected = true;
+    if (pageId === 'paginaAhorro' || pageId === 'paginaHome') {
+        document.getElementById('totalAhorros').textContent = 'Total de ahorros: $' + totalAhorros.toFixed(2);
+        actualizarMontoActual(); 
+     }else{
+        const yearSelect = document.getElementById('inputGroupSelectYear');
+        const currentYear = new Date().getFullYear();
+        const startYear = 2024;
+        const endYear = currentYear + 2;
+
+        
+        for (let year = startYear; year <= endYear; year++) {
+            let option = document.createElement('option');
+            option.value = year;
+            option.textContent = year;
+            if (year === currentYear) {
+                option.selected = true;
+            }
+            yearSelect.appendChild(option);
         }
-        yearSelect.appendChild(option);
-    }
 
-    yearSelect.addEventListener('change', function() {
+        yearSelect.addEventListener('change', function() {
+            yearSeleccionado = parseInt(yearSelect.value, 10);
+            actualizarMontoActual();
+            
+            if (pageId === 'paginaResumen') {
+                resumenAnual(); 
+            }
+        });
+
         yearSeleccionado = parseInt(yearSelect.value, 10);
         actualizarMontoActual();
-    });
 
-    actualizarMontoActual();
+        
+        if (pageId === 'paginaResumen') {
+            resumenAnual(); 
+        }
+    }
 });
 
 function agregarIngreso() {
-  let mes = document.getElementById("inputGroupSelect01").value;
-  let porcentajeAhorro = parseFloat(document.getElementById("inputGroupSelectPorcentaje").value);
+    let mes = document.getElementById("inputGroupSelect01").value;
+    let porcentajeAhorro = parseFloat(document.getElementById("inputGroupSelectPorcentaje").value);
 
-  if (mes === '' || mes === 'Selecciona el mes') {
-      alert('Por favor, selecciona un mes.');
-      return;
-  }
+    if (mes === '' || mes === 'Selecciona el mes') {
+        alert('Por favor, selecciona un mes.');
+        return;
+    }
 
-  let descIngreso = document.getElementById("descripcionIngreso").value.trim();
-  let input = document.getElementById("montoIngreso");
-  let ingreso = parseFloat(input.value);
+    let descIngreso = document.getElementById("descripcionIngreso").value.trim();
+    let input = document.getElementById("montoIngreso");
+    let ingreso = parseFloat(input.value);
 
-  if (isNaN(ingreso) || ingreso <= 0) {
-      alert('Por favor, ingresa un monto válido.');
-      return;
-  }
+    if (isNaN(ingreso) || ingreso <= 0) {
+        alert('Por favor, ingresa un monto válido.');
+        return;
+    }
 
-  // Calcular y registrar el ahorro
-  let ahorro = (ingreso * porcentajeAhorro) / 100;
-  let ingresoNeto = ingreso - ahorro; // Monto después de deducir el ahorro
+    let ahorro = (ingreso * porcentajeAhorro) / 100;
+    let ingresoNeto = ingreso - ahorro; 
 
-  // Actualizar ingresos por mes
-  let claveMesAño = `${mes}-${yearSeleccionado}`;
-  if (!ingresosPorMes[claveMesAño]) {
-      ingresosPorMes[claveMesAño] = [];
-  }
+    let claveMesAño = `${mes}-${yearSeleccionado}`;
+    if (!ingresosPorMes[claveMesAño]) {
+        ingresosPorMes[claveMesAño] = [];
+    }
 
-  ingresosPorMes[claveMesAño].push({ descripcion: descIngreso, monto: ingresoNeto });
-  montoActual += ingresoNeto; // Solo sumar el monto neto al total actual
+    ingresosPorMes[claveMesAño].push({ descripcion: descIngreso, monto: ingresoNeto });
+    montoActual += ingresoNeto; 
 
-  // Actualizar ahorros por mes
-  if (!ahorrosPorMes[claveMesAño]) {
-      ahorrosPorMes[claveMesAño] = 0;
-  }
-  ahorrosPorMes[claveMesAño] += ahorro;
+    if (!ahorrosPorMes[claveMesAño]) {
+        ahorrosPorMes[claveMesAño] = 0;
+    }
+    ahorrosPorMes[claveMesAño] += ahorro;
+    document.getElementById('totalAhorros').textContent = 'Total de ahorros: $' + totalAhorros.toFixed(2);
 
-  // Actualizar valores en localStorage
-  localStorage.setItem('ingresosPorMes', JSON.stringify(ingresosPorMes));
-  localStorage.setItem('montoActual', montoActual.toString());
-  localStorage.setItem('totalAhorros', (totalAhorros + ahorro).toString());
-  localStorage.setItem('ahorrosPorMes', JSON.stringify(ahorrosPorMes));
+    localStorage.setItem('ingresosPorMes', JSON.stringify(ingresosPorMes));
+    localStorage.setItem('montoActual', montoActual.toString());
+    localStorage.setItem('totalAhorros', (totalAhorros + ahorro).toString());
+    localStorage.setItem('ahorrosPorMes', JSON.stringify(ahorrosPorMes));
 
-  actualizarMontoActual(); // Actualizar el monto y el ahorro en el DOM
+    actualizarMontoActual(); 
+    document.getElementById("inputGroupSelect01").value = 'Selecciona el mes';
+    document.getElementById("descripcionIngreso").value = '';
+    document.getElementById("montoIngreso").value = '';
+    document.getElementById("inputGroupSelectPorcentaje").value = '0';
 
-  // Limpiar campos
-  document.getElementById("inputGroupSelect01").value = 'Selecciona el mes';
-  document.getElementById("descripcionIngreso").value = '';
-  document.getElementById("montoIngreso").value = '';
-  document.getElementById("inputGroupSelectPorcentaje").value = '0';
-}
+ }
 
 function obtenerIngresosPorMes(mes) {
-  return ingresosPorMes[`${mes}-${yearSeleccionado}`] || [];
+    return ingresosPorMes[`${mes}-${yearSeleccionado}`] || [];
 }
-
 
 function consultarIngreso() {
     let mes = document.getElementById("inputGroupSelect01").value;
@@ -98,7 +115,7 @@ function consultarIngreso() {
         return;
     }
 
-    let ingresos = obtenerIngresosPorMes(mes);
+    let ingresosMes = obtenerIngresosPorMes(mes);
     resultadosDiv.innerHTML = '';
 
     let contenido = `<table class="table table-striped table-hover">
@@ -108,7 +125,7 @@ function consultarIngreso() {
                             </tr>   
                         </thead>`;
 
-    if (ingresos.length === 0) {
+    if (ingresosMes.length === 0) {
         resultadosDiv.innerHTML = contenido + `<thead>
                             <tr>
                                 <td><p>El mes seleccionado no posee ingresos</p></td>
@@ -129,7 +146,7 @@ function consultarIngreso() {
 
         let sumaIngresos = 0;
 
-        ingresos.forEach(ingreso => {
+        ingresosMes.forEach(ingreso => {
             tabla += `
                 <tr>
                     <td><i class="bi bi-dot"></i></td>
@@ -159,30 +176,196 @@ function consultarIngreso() {
 }
 
 function actualizarMontoActual() {
-  montoActual = parseFloat(localStorage.getItem('montoActual')) || 0;
-  totalAhorros = parseFloat(localStorage.getItem('totalAhorros')) || 0;
+    montoActual = parseFloat(localStorage.getItem('montoActual')) || 0;
+    totalAhorros = parseFloat(localStorage.getItem('totalAhorros')) || 0;
 
-  document.getElementById('montoActual').innerHTML = 'Monto actual: $' + montoActual.toFixed(2);
-  document.getElementById('totalAhorros').innerHTML = 'Total de ahorros: $' + totalAhorros.toFixed(2);
-
-}
+    document.getElementById('montoActual').textContent = 'Monto actual: $' + montoActual.toFixed(2);
+    
+  }
 function extraerAhorro() {
-  let cantidadAextraer = prompt("¿Cuánto deseas extraer del ahorro?", "0");
-  cantidadAextraer = parseFloat(cantidadAextraer);
+    let cantidadAextraer = prompt("¿Cuánto deseas extraer del ahorro?", "0");
+    cantidadAextraer = parseFloat(cantidadAextraer);
 
-  if (isNaN(cantidadAextraer) || cantidadAextraer <= 0) {
-      alert('Por favor, ingresa una cantidad válida.');
-      return;
-  }
+    if (isNaN(cantidadAextraer) || cantidadAextraer <= 0) {
+        alert('Por favor, ingresa una cantidad válida.');
+        return;
+    }
 
-  if (cantidadAextraer > totalAhorros) {
-      alert('No tienes suficiente ahorro para extraer esa cantidad.');
-      return;
-  }
+    if (cantidadAextraer > totalAhorros) {
+        alert('No tienes suficiente ahorro para extraer esa cantidad.');
+        return;
+    }
 
-  totalAhorros -= cantidadAextraer;
-  localStorage.setItem('totalAhorros', totalAhorros.toString());
-
-  // Actualizar el resultado de ahorros en el DOM
-  actualizarMontoActual();
+    totalAhorros -= cantidadAextraer;
+    localStorage.setItem('totalAhorros', totalAhorros.toString());
+    actualizarMontoActual();
 }
+
+function agregarGasto() {
+    let mes = document.getElementById("inputGroupSelect01").value;
+
+    if (mes === '' || mes === 'Selecciona el mes') {
+        alert('Por favor, selecciona un mes.');
+        return;
+    }
+
+    let descGasto = document.getElementById("descripcionGasto").value.trim();
+    let input = document.getElementById("montoGasto");
+    let gasto = parseFloat(input.value);
+
+    if (isNaN(gasto) || gasto <= 0) {
+        alert('Por favor, ingresa un monto válido.');
+        return;
+    }
+
+    let claveMesAño = `${mes}-${yearSeleccionado}`;
+    if (!gastosPorMes[claveMesAño]) {
+        gastosPorMes[claveMesAño] = [];
+    }
+
+    gastosPorMes[claveMesAño].push({ descripcion: descGasto, monto: gasto });
+    montoActual -= gasto; 
+
+    localStorage.setItem('gastosPorMes', JSON.stringify(gastosPorMes));
+    localStorage.setItem('montoActual', montoActual.toString());
+
+    actualizarMontoActual(); 
+
+    document.getElementById("inputGroupSelect01").value = 'Selecciona el mes';
+    document.getElementById("descripcionGasto").value = '';
+    document.getElementById("montoGasto").value = '';
+}
+
+function obtenerGastosPorMes(mes) {
+    return gastosPorMes[`${mes}-${yearSeleccionado}`] || [];
+}
+
+function consultarGasto() {
+    let mes = document.getElementById("inputGroupSelect01").value;
+    let resultadosDiv = document.getElementById('resultadosGastos');
+
+    if (mes === '' || mes === 'Selecciona el mes') {
+        alert('Por favor, selecciona un mes.');
+        return;
+    }
+
+    let gastosMes = obtenerGastosPorMes(mes);
+    resultadosDiv.innerHTML = '';
+
+    let contenido = `<table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <td><h4>Gastos de ${mes} ${yearSeleccionado}</h4></td>
+                            </tr>   
+                        </thead>`;
+
+    if (gastosMes.length === 0) {
+        resultadosDiv.innerHTML = contenido + `<thead>
+                            <tr>
+                                <td><p>El mes seleccionado no posee gastos</p></td>
+                            </tr>   
+                        </thead>`;
+    } else {
+        let tabla = `
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <th>Descripción</th>
+                        <th>Monto</th>
+                    </tr>
+                </thead>
+                <tbody>
+        `;
+
+        let sumaGastos = 0;
+
+        gastosMes.forEach(gasto => {
+            tabla += `
+                <tr>
+                    <td><i class="bi bi-dot"></i></td>
+                    <td>${gasto.descripcion}</td>
+                    <td>$${gasto.monto.toFixed(2)}</td>
+                </tr>
+            `;
+            sumaGastos += gasto.monto; 
+        });
+
+        tabla += `
+                </tbody>
+                <tfoot class="table-dark">
+                    <tr>
+                        <td>Total</td>
+                        <td></td>
+                        <td>$${sumaGastos.toFixed(2)}</td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+
+        resultadosDiv.innerHTML = contenido + tabla;
+    }
+
+    document.getElementById("inputGroupSelect01").value = '';
+    actualizarMontoActual();
+}
+
+function resumenAnual() {
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    
+    let resumenMeses = meses.map(mes => {
+        let claveMesAño = `${mes}-${yearSeleccionado}`;
+        
+        let ingresosMes = obtenerIngresosPorMes(mes);
+        let gastosMes = obtenerGastosPorMes(mes);
+        let ahorrosMes = ahorrosPorMes[claveMesAño] || 0;
+
+        let totalIngresosMes = ingresosMes.reduce((acc, ingreso) => acc + ingreso.monto, 0);
+        let totalGastosMes = gastosMes.reduce((acc, gasto) => acc + gasto.monto, 0);
+
+        return {
+            mes,
+            ingresos: totalIngresosMes,
+            gastos: totalGastosMes,
+            ahorros: ahorrosMes,
+            restoTotal: totalIngresosMes - totalGastosMes
+        };
+    });
+
+    
+    let resumenDiv = document.getElementById('resumenAnual');
+    resumenDiv.innerHTML = `<table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <td><h4> Resumen Anual ${yearSeleccionado}</h4></td>
+                            </tr>   
+                        </thead>
+       <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Concepto</th>
+                    ${meses.map(mes => `<th>${mes}</th>`).join('')}
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Ingresos</td>
+                    ${resumenMeses.map(mesResumen => `<td>$${mesResumen.ingresos.toFixed(2)}</td>`).join('')}
+                </tr>
+                <tr>
+                    <td>Gastos</td>
+                    ${resumenMeses.map(mesResumen => `<td>$${mesResumen.gastos.toFixed(2)}</td>`).join('')}
+                </tr>
+                <tr>
+                    <td>Ahorros</td>
+                    ${resumenMeses.map(mesResumen => `<td>$${mesResumen.ahorros.toFixed(2)}</td>`).join('')}
+                </tr>
+                <tr>
+                    <td>Resto del mes</td>
+                    ${resumenMeses.map(mesResumen => `<td>$${mesResumen.restoTotal.toFixed(2)}</td>`).join('')}
+                </tr>
+            </tbody>
+        </table>
+    `;
+}
+
